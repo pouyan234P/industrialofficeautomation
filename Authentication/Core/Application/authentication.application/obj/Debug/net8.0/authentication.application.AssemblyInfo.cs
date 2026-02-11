af850b2e151ecad5fb2738be470bb1807071194a
Pouyan Parvaresh <pouyan234@gmail.com>
2026-02-11 22:09:34 +0330

    start application for cleanarchitecture in correspondencecore servic

--- Authentication/Core/Application/authentication.application/obj/Debug/net8.0/authentication.application.AssemblyInfo.cs
+++ Authentication/Core/Application/authentication.application/obj/Debug/net8.0/authentication.application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("authentication.application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5139d10803bbb805859a4a76ad615bb23807b0e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a6cef8ead37e32bec3148c70610572f04a26f8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("authentication.application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("authentication.application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ automationCorrespondencecoreService/core/Application/CorrespondenceCore.Application/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CorrespondenceCore.Application
+{
+    public class Class1
+    {
+
+    }
+}
